9f33d6844d3884ce740a74a00a4f0837323899c1
mahedihsharif <mahedi5061@gmail.com>
2024-07-28 15:39:21 +0600

    update

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env("HOST", "0.0.0.0"), // Sets the public URL of the application.
+  url: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  // url: env("HOST"), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
   },
